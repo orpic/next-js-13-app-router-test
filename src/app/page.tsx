3855affe46730857e7ccc342cdcafe0b8fa74160
shobhit <shobhitumrao5@gmail.com>
2023-09-05 23:32:12 +0530

    route one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Heading } from "@/components";
-import Image from "next/image";
 
 export default function Home() {
   return <Heading />;
---
+++ src/app/test-route-one/page.tsx
@@ -0,0 +1,3 @@
+export default function TestRouteOne() {
+  return <>Hello from test route one</>;
+}
